b5ba82d31abd1b495a6bcd2c3d72f80e04f69179
David Klimaszewski <DaKlimaszewski@gmail.com>
2024-03-17 12:27:11 -0400

    some tweaks

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let mut query = "";
-    if let Some(val) = &args.get(1) {
-        query = val;
+    let parsed_results = parse_config(&args);
+
+    println!("Searching for {}", parsed_results.query);
+    println!("In file {}", parsed_results.file_path);
+
+    let contents = fs::read_to_string(parsed_results.file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let mut query = String::from("");
+    let mut file_path = String::from("");
+
+    if let Some(val) = args.get(1) {
+        query = val.clone();
     }
 
-    let mut file_path = "";
-    if let Some(val) = &args.get(2) {
-        file_path = val;
+    if let Some(val) = args.get(2) {
+        file_path = val.clone();
     }
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    Config { query, file_path }
 }

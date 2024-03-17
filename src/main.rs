95e081cf2055e0ad79cd9cdbd9a504774ff8b917
David Klimaszewski <DaKlimaszewski@gmail.com>
2024-03-17 17:38:27 -0400

    better

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use minigrep::{run, Config};
+use minigrep::Config;
 use std::{env, process};
 
 fn main() {
@@ -21,7 +21,7 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }

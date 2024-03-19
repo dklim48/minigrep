9d59e1d3879736495061b07c3bdcca85e6d25b6c
David Klimaszewski <DaKlimaszewski@gmail.com>
2024-03-18 22:20:57 -0400

    added the search

--- src/lib.rs
+++ src/lib.rs
@@ -43,11 +43,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
 #[cfg(test)]
 mod config_tests {
-    use crate::Config;
-
-    // use super::*;
+    use super::*;
 
     #[test]
     fn constructor_works() {
@@ -56,5 +64,16 @@ mod config_tests {
 
         assert_eq!(new_config.query, "");
         assert_eq!(new_config.file_path, "");
+    }
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
